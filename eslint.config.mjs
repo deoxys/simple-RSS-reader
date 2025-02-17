97be01b50bd9c9b6430dc8bb510963c7a15f7443
Giovan Angela <giovan.angela@gmail.com>
2025-02-17 20:59:25 +0100

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,33 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: { import: importPlugin },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "external",
+            "builtin",
+            "internal",
+            "sibling",
+            "parent",
+            "index",
+          ],
+          pathGroups: [
+            { pattern: "components", group: "internal" },
+            { pattern: "common", group: "internal" },
+            { pattern: "routes/**", group: "internal" },
+            { pattern: "assets/**", group: "internal", position: "after" },
+          ],
+          pathGroupsExcludedImportTypes: ["internal"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
